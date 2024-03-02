454996ebf0fdcae4fe8c0ac3adbcf4da4a72fd2d
oka <keeaun_96@yahoo.com>
2024-03-03 04:30:28 +0800

    add Point file

---
+++ geometry/src/main/java/Point.java
@@ -0,0 +1,26 @@
+import com.algo.geometry.constant.GeometryConstant;
+
+/**
+ * @author oka
+ * @date 03/03/2024
+ */
+public class Point implements Comparable<Point> {
+    private double x;
+    private double y;
+
+    Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point p){
+        if(Math.abs(x-p.x) > GeometryConstant.EPS){
+            return (x < p.x) ? -1 : 1;
+        } else if(Math.abs(y-p.y) > GeometryConstant.EPS){
+            return (y < p.y) ? -1 : 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ geometry/src/main/java/com/algo/geometry/constant/GeometryConstant.java
@@ -0,0 +1,9 @@
+package com.algo.geometry.constant;
+
+/**
+ * @author oka
+ * @date 03/03/2024
+ */
+public class GeometryConstant {
+    public static double EPS = 0.000000001;
+}
