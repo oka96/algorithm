afdd6d57a2f23e57f08f87ed975db9f53304d438
oka <keeaun_96@yahoo.com>
2024-03-03 05:46:04 +0800

    add Point test case

--- geometry/src/test/java/com/algo/geometry/PointTest.java
+++ geometry/src/test/java/com/algo/geometry/PointTest.java
@@ -3,13 +3,10 @@ package com.algo.geometry;
 import com.algo.geometry.constant.GeometryConstant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.text.MessageFormat;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 /**
  * @author oka
